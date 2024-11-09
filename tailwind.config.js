5532ddf430a26b28cc71503a6970a57557679a94
auroraloreti <loretiaurora16@gmail.com>
2024-11-09 15:50:20 +0100

    theme aggiunto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      extend: {},
+    },
+    colors: {
+      purple: "#3D0E96ff",
+      coral: "#F27F4Cff",
+      periwinkle: "#D1D7F7ff",
+      "dark-blue": "#24056Fff",
+      liliac: "#9736E4ff",
+
+      plugins: [require("flowbite/plugin")],
+    },
+  },
+};
