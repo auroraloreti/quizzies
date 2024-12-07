9b9898e70538fce76306d39ba2524992b935ab17
auroraloreti <loretiaurora16@gmail.com>
2024-12-07 15:56:56 +0100

    barra di caricamento

--- src/Home/Achievement.jsx
+++ src/Home/Achievement.jsx
@@ -1,21 +1,22 @@
-export default function Achievement () {
+export default function Achievement() {
+  return (
+    <>
+      <div className=" bg-coral  rounded-full w-100% h-60 mb-6 flex flex-row">
+        <div className="flex items-center ">
+          <div className=" w-44 h-44 bg-lilac rounded-full ml-8 mt-3"></div>
 
-    
-    
-    return(
-        <>
-        <div className=" bg-coral  rounded-full w-100% h-60 mb-6 flex flex-row">
-            <div className="flex items-center ">
-            <div  className=" w-44 h-44 bg-lilac rounded-full ml-8 mt-8"  >
-         </div>
+          <div className=" ml-7 mt-8 text-xl">
+            <p className="text-white mb-4 text-3xl"> novizio dei quiz</p>
+            <p className="text-white">completa 2 quiz</p>
+            <div className="progress-bar bg-white rounded-full h-4 mt-8  ">
+              <div
+                className="progress-fill bg-lilac rounded-full h-4 "
+                style={{ width: "50%" }}
+              ></div>
             </div>
-            <div className=" ml-7 mt-8 text-xl" >
-                <p className="text-white"> novizio dei quiz</p>
-                <p className="text-white">completa 2 quiz</p>
-                
-
-            </div>
+          </div>
         </div>
-        </>
-    )
-};
+      </div>
+    </>
+  );
+}
