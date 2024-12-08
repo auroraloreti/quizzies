f0640987b00e1af8f177e9b416703e6c088de4a6
Andros924 <sir_vivi@hotmail.it>
2024-12-08 15:44:33 +0100

    migliorie

--- src/Home/Achievement.jsx
+++ src/Home/Achievement.jsx
@@ -1,22 +1,19 @@
 export default function Achievement() {
   return (
-    <>
-      <div className=" bg-coral  rounded-full w-100% h-60 mb-6 flex flex-row">
-        <div className="flex items-center ">
-          <div className=" w-44 h-44 bg-lilac rounded-full ml-8 mt-3"></div>
+    <div className="bg-coral rounded-full w-full h-auto mb-6 p-8 flex flex-col items-center md:flex-row md:items-center">
+      {/* Segnaposto per immagine (attualmente viola) */}
+      <div className="w-32 h-32 bg-lilac rounded-full mb-4 md:mb-0 md:mr-8 flex-shrink-0"></div>
 
-          <div className=" ml-7 mt-8 text-xl">
-            <p className="text-white mb-4 text-3xl"> novizio dei quiz</p>
-            <p className="text-white">completa 2 quiz</p>
-            <div className="progress-bar bg-white rounded-full h-4 mt-8  ">
-              <div
-                className="progress-fill bg-lilac rounded-full h-4 "
-                style={{ width: "50%" }}
-              ></div>
-            </div>
-          </div>
+      <div className="flex flex-col justify-center flex-grow text-center md:text-left">
+        <p className="text-white text-3xl font-semibold mb-2">
+          Novizio dei quiz
+        </p>
+        <p className="text-white mb-4">Completa 2 quiz</p>
+
+        <div className="bg-white rounded-full h-4 w-full overflow-hidden">
+          <div className="bg-lilac h-4" style={{ width: "50%" }}></div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/Home/Carosello.jsx
+++ src/Home/Carosello.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import "tailwindcss/tailwind.css";
 
 function Carousel() {
   const squares = Array.from({ length: 10 }, (_, index) => (
-    <div key={index} className="flex flex-col items-center">
-      <div className="w-20 h-20 bg-lilac rounded-full mb-2"  ></div>
-      <p className="text-center text-purple ">{index + 1} posto</p>
+    <div key={index} className="flex flex-col items-center flex-shrink-0">
+      <div
+        className="
+          w-16 h-16 sm:w-20 sm:h-20 
+          bg-lilac rounded-full mb-2
+        "
+      ></div>
+      <p className="text-center text-purple text-sm sm:text-base">
+        {index + 1}° posto
+      </p>
     </div>
   ));
 
   return (
-    <div className="flex overflow-x-auto space-x-4 mb-5">
+    <div
+      className="
+      w-full 
+      flex 
+      space-x-4 
+      overflow-x-auto 
+      py-4
+      scrollbar-thin scrollbar-track-transparent scrollbar-thumb-purple-300
+    "
+    >
       {squares}
     </div>
   );
